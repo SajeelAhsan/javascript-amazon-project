c174f6e4baa84fb5f527fac12d8b118742c4e17c
SajeelAhsan <sajeel.ahsan2298@gmail.com>
2024-12-03 16:19:16 +0100

    Move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,21 @@
 export const cart = [];
+
+//This function will allows us to add products into the cart
+export function addToCart(productId, quantity) {
+  let matchingItem;
+    cart.forEach((cartItem) => {
+      if(productId === cartItem.productid)
+      {
+        matchingItem = cartItem;
+      }
+    });
+    if(matchingItem) {
+      matchingItem.quantity += quantity;
+    }
+    else {
+      cart.push({ 
+        productId,
+        quantity 
+      });
+    }
+ }
