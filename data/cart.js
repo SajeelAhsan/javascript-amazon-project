cbf3d952fe44cc82fe087847445405296aed0538
SajeelAhsan <sajeel.ahsan2298@gmail.com>
2024-12-06 15:03:53 +0100

    Checkout items are updated according to Cart Quantity

--- data/cart.js
+++ data/cart.js
@@ -19,7 +19,7 @@ function saveToStorage(){
 export function addToCart(productId, quantity) {
   let matchingItem;
     cart.forEach((cartItem) => {
-      if(productId === cartItem.productid)
+      if(productId === cartItem.productId)
       {
         matchingItem = cartItem;
       }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -101,5 +101,14 @@ cart.forEach((cartItem) => {
       const container = document.querySelector(
         `.js-cart-item-container-${productId}`);
         container.remove();
+        updateCartQuantity();
     });
   });
+function updateCartQuantity(){
+  let cartQuantity = 0 ;
+    cart.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity;
+    });
+    document.querySelector('.js-return-to-home-link').innerHTML = `${cartQuantity} items`;
+ }
+ updateCartQuantity();
