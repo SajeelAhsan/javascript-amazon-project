1ef1a08cc39a81c5b98a2ca62acb872aa5ab28ed
SajeelAhsan <sajeel.ahsan2298@gmail.com>
2024-12-07 21:39:17 +0100

    delivery date updated

---
+++ data/deliveryOptions.js
@@ -0,0 +1,17 @@
+ export const deliveryOption = [
+  {
+    id: '1',
+    deliveryTime: '7 days',
+    deliveryPrice: 0
+  },
+  {
+    id: '2',
+    deliveryTime: '3 days',
+    deliveryPrice: 499
+  },
+  {
+    id: '3',
+    deliveryTime: '1 days',
+    deliveryPrice: 999
+  }
+];
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,8 @@
 import { formatCurrency } from './utils/money.js';
 import { cart, claculateCartQuantity, removeFromCart, updateQuantity } from '../data/cart.js';
 import { products } from '../data/products.js';
-
+import { deliveryOption } from '../data/deliveryOptions.js';
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 let cartSummaryHtml = '';
 cart.forEach((cartItem) => {
   const productId = cartItem.productId;
@@ -49,50 +50,46 @@ cart.forEach((cartItem) => {
                 <div class="delivery-options-title">
                   Choose a delivery option:
                 </div>
-                <div class="delivery-option">
-                  <input type="radio" checked
-                    class="delivery-option-input"
-                    name="delivery-option-${matchingProduct.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Tuesday, June 21
-                    </div>
-                    <div class="delivery-option-price">
-                      FREE Shipping
-                    </div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-${matchingProduct.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Wednesday, June 15
-                    </div>
-                    <div class="delivery-option-price">
-                      $4.99 - Shipping
-                    </div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-${matchingProduct.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Monday, June 13
-                    </div>
-                    <div class="delivery-option-price">
-                      $9.99 - Shipping
-                    </div>
-                  </div>
-                </div>
+               ${deliveryOptionsHTML(matchingProduct)}
               </div>
             </div>
           </div>
 `;
 });
+function deliveryOptionsHTML(matchingProduct){
+
+  let html = '';
+
+  deliveryOption.forEach((deliveryOption) => {
+  const today = dayjs();
+  console.log(today)
+  const deliveryDate = today.add(deliveryOption.deliveryDays, 'day');
+  console.log(deliveryDate)
+  const dateString = deliveryDate.format('dddd-MMM-D');
+  const priceString = deliveryOption.priceCents
+   === 0
+   ? 'FREE'
+   : `$${formatCurrency(deliveryOption.priceCents / 100)} -`;
+
+  html += `
+  <div class="delivery-option">
+      <input type="radio"
+        class="delivery-option-input"
+          name="delivery-option-${matchingProduct.id}">
+     <div>
+      <div class="delivery-option-date">
+         ${dateString}
+       </div>
+     <div class="delivery-option-price">
+        ${priceString} Shipping
+      </div>
+    </div>
+  </div>
+  `
+  });
+
+  return html;
+}
  document.querySelector('.js-order-summary').innerHTML = cartSummaryHtml;
  document.querySelectorAll('.js-delete-link')
   .forEach((link) => {
@@ -138,7 +135,7 @@ function updateCartQuantity(){
 
       const container = document.querySelector(`.js-cart-item-container-${productId}`);
       container.classList.remove('is-editing-quantity');
-      
+
       const quantityLabel = document.querySelector(`.js-quantity-label-${productId}`);
       quantityLabel.innerHTML = newQuantity;
       updateCartQuantity();
