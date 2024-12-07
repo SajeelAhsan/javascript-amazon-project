bff8223f1b370dc2e6be7920232c3a16a6062038
SajeelAhsan <sajeel.ahsan2298@gmail.com>
2024-12-07 15:58:17 +0100

    Update Button is Intractive now

--- data/cart.js
+++ data/cart.js
@@ -56,4 +56,17 @@ export function addToCart(productId, quantity) {
     });
   return cartQuantity;
  }
+ export function updateQuantity(productId, newQuantity) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.quantity = newQuantity;
+
+  saveToStorage();
+}
  
